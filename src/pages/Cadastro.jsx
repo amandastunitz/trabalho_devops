ea6d69c95b4d59237df892ef60d347751474d1a1
amandastunitz <amandastunitz2@gmail.com>
2026-09-18 22:16:31 -0300

    feat: adiciona paginas e rotos

---
+++ src/pages/Cadastro.jsx
@@ -0,0 +1,13 @@
+import {Link} from 'react-router-dom'
+
+function Cadastro () {
+    return (
+        <section className="cartao-login">
+            <h1> Cadastro </h1>
+            <p>O </p>
+            <Link to="/login">Já tenho conta</Link>
+        </section>
+    )
+}
+
+export default Cadastro 
---
+++ src/pages/Principal.jsx
@@ -0,0 +1,13 @@
+import {Link} from 'react-router-dom'
+
+function Principal () {
+    return (
+        <section className="cartao-login">
+            <h1>Principal</h1>
+            <p>o</p>
+            <Link to="login">Voltar ao login</Link>
+        </section>
+    )
+}
+
+export default Principal
